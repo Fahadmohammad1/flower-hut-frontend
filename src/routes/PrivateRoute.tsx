893c1c89fac36c59a1c1dff1878aab4dc79767a3
Fahad <205fahadmohammad@gmail.com>
2024-06-05 22:30:05 +0600

    private route error fixed

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,8 +1,9 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import { auth } from "../firebase/firebase.config";
+import { ReactNode } from "react";
 
-export default function PrivateRoute({ children }) {
+export default function PrivateRoute({ children }: { children: ReactNode }) {
   const [user, loading] = useAuthState(auth);
   const location = useLocation();
   if (loading) {
